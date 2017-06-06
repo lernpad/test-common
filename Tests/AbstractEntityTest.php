3fbd5f7be68dd3fb411d0f31d8112bbd15ccf6bd
Andrey Chernov <lernpad@mail.ru>
2017-06-06 16:20:10 +0700

    add new parameter in validate method bool $result - expected result of validation check to pass test

--- Tests/AbstractEntityTest.php
+++ Tests/AbstractEntityTest.php
@@ -4,15 +4,15 @@ namespace Lernpad\TestBundle\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-abstract class AbstractEntityTest extends KernelTestCase {
-
+abstract class AbstractEntityTest extends KernelTestCase
+{
     public function setUp()
     {
         static::bootKernel();
     }
 
     /**
-     * Get manager
+     * Get manager.
      *
      * @return \Doctrine\ORM\EntityManager
      */
@@ -22,20 +22,42 @@ abstract class AbstractEntityTest extends KernelTestCase {
     }
 
     /**
-     * Validate object
+     * Validate object.
      *
      * @param object $entity
-     * @param array $validators
+     * @param bool   $result expected result of validation check
+     * @param array  $groups validation groups
      */
-    protected function validate($entity, $groups = [])
+    protected function validate($entity, $result, $groups = [])
+    {
+        $errors = static::$kernel->getContainer()->get('validator')->validate($entity, null, $groups);
+
+        $valid = (count($errors) <= 0);
+
+        if ($result !== $valid) {
+            foreach ($errors as $error) {
+                echo $error->getMessage().' for \''.$error->getPropertyPath().'\' in '.get_class($entity)."\n";
+            }
+        }
+        $this->assertSame($result, $valid, 'Validation Test Fail');
+    }
+
+    /**
+     * Validate object for one type.
+     *
+     * @param object $entity
+     * @param bool   $result expected result of validation check
+     * @param array  $groups validation groups
+     */
+    protected function validateOne($entity, $result, $groups = [])
     {
         $errors = static::$kernel->getContainer()->get('validator')->validate($entity, null, $groups);
 
         if (count($errors) > 0) {
             foreach ($errors as $error) {
-                echo $error->getMessage() . ' for \'' . $error->getPropertyPath() . '\' in ' . get_class($entity) . "\n";
+                echo $error->getMessage().' for \''.$error->getPropertyPath().'\' in '.get_class($entity)."\n";
             }
         }
-        $this->assertEquals(0, count($errors), "Validation Fail");
+        $this->assertSame((int) !$result, count($errors), 'Validation Fail');
     }
 }
